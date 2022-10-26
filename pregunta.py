d21e72f2661bd24126aa06c57633481761632b67
juaelopezlop <juaelopezlop@unal.edu.co>
2022-10-26 14:51:41 -0500

    Correcciones

--- pregunta.py
+++ pregunta.py
@@ -12,43 +12,43 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    # df = pd.read_csv("C:/Users/jlopezl/OneDrive - Renting Colombia S.A/Archivos/Personal/Especialización/Ciencia de los datos/data-cleaning-solicitudes-credito-JuanesLopez/solicitudes_credito.csv", sep=";")
+
     #
     # Inserte su código aquí
     #
+    #print(df.dtypes)
+
+    #eliminar datos faltantes y duplicados
     df.dropna(inplace=True)
-    df.drop(['Unnamed: 0'], axis=1, inplace=True)
     
-    df.sexo = df.sexo.str.lower()
+
+    #volver todo minusculas y eliminar espacios al inicio y al final
+    df["sexo"] = df["sexo"].apply(lambda x: str(x).lower().strip())
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].apply(lambda x: str(x).lower().strip())
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).lower().strip())
+    #df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().strip())
+    #df["estrato"] = df["estrato"].apply(lambda x: str(x).lower().strip())
+    #df["comuna_ciudadano"] = df["comuna_ciudadano"].apply(lambda x: str(x).lower().strip())
+    #df["fecha_de_beneficio"] = df["fecha_de_beneficio"].apply(lambda x: str(x).lower().strip())
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).lower().strip())
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().strip())
+
+
+    #reemplazar guiones por espacios
+    df["idea_negocio"] = df["idea_negocio"].apply(lambda x: str(x).replace("-"," ").replace("_"," ").strip())
+    df["barrio"] = df["barrio"].apply(lambda x: str(x).lower().replace("_"," ").replace("-"," "))
+    df["línea_credito"] = df["línea_credito"].apply(lambda x: str(x).lower().replace("-", " ").replace("_", " ").strip())
+
+    #modificar formato fecha
+    df["fecha_de_beneficio"] = pd.to_datetime(df["fecha_de_beneficio"], dayfirst=True)
+
+    #monto eliminar signo $, y limpiar
+    df["monto_del_credito"] = df["monto_del_credito"].apply(lambda x: str(x).strip("$").strip().replace(".00", "").replace(",", ""))
     
-    df.tipo_de_emprendimiento = df.tipo_de_emprendimiento.str.lower()
+    #eliminar columna Unnamed del dataFrame
+    df.drop(['Unnamed: 0'], axis=1,inplace=True)
     
-    df.idea_negocio = [i.strip() for i in df.idea_negocio]
-    df.idea_negocio = df.idea_negocio.str.replace("-"," ")
-    df.idea_negocio = df.idea_negocio.str.replace("_"," ")
-    df.idea_negocio = df.idea_negocio.str.lower()
-    
-    df.barrio = [str(i).strip() for i in df.barrio]
-    df.barrio = df.barrio.str.replace("-"," ")
-    df.barrio = df.barrio.str.replace("_"," ")
-    df.barrio = df.barrio.str.lower()
-    
-    
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio)
-    df.fecha_de_beneficio = df.fecha_de_beneficio.astype('datetime64[ns]')
-    
-    df.monto_del_credito = df.monto_del_credito.str.replace("%","")
-    df.monto_del_credito = df.monto_del_credito.str.replace("$","")
-    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
-    df.monto_del_credito = df.monto_del_credito.str.replace(".00","") 
-    df.monto_del_credito = [i.strip() for i in df.monto_del_credito]
-    
-    df.línea_credito = df.línea_credito.str.replace("-"," ")
-    df.línea_credito = df.línea_credito.str.replace("_"," ")
-    df.línea_credito = df.línea_credito.str.lower()
-    df.línea_credito = [i.strip() for i in df.línea_credito]
-    
-    df.dropna(inplace=True)
     df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
 
     return df
